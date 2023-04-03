392ed397f618ce344ef22f6a4a79d4897b6b1e9a
mstranger <mr.mstranger@gmail.com>
2023-04-04 00:13:17 +0300

    User resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_180742) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_211019) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "deadline"
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_180742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_tasks_on_title", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
