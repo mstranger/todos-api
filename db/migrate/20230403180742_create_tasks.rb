46f558797fe932dd26ec5924027654e772f79560
mstranger <mr.mstranger@gmail.com>
2023-04-03 20:43:43 +0300

    Task model

---
+++ db/migrate/20230403180742_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false, index: { unique: true }
+      t.datetime :deadline
+      t.string :prior
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
