d0e3a7b1ae187c0e3f39ecf912d40360a4ff1cd8
mstranger <mr.mstranger@gmail.com>
2023-04-05 13:27:59 +0300

    Create and delete task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,34 @@ class Api::V1::TasksController < Api::V1::ApiController
 
     @task = Task.find(params[:id])
   end
+
+  def create
+    # puts "----"
+    # pp task_params
+    # puts "----"
+
+    @task = Task.new(task_params)
+    if @task.save
+      render json: { result: :success }, status: :created
+    else
+      render json: { result: :fail, errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    # debugger
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    render json: { result: :success }, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { result: :fail }, status: :not_found
+  end
+
+  private
+
+  def task_params
+    params.require(:data).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :tasks, only: [:index, :show]
+      resources :tasks, only: [:index, :show, :create, :destroy]
     end
   end
 end
