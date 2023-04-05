a450b03279e31220e74c9a27e5e8036eabe1895d
mstranger <mr.mstranger@gmail.com>
2023-04-05 14:05:05 +0300

    Update tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,29 +1,30 @@
 class Api::V1::TasksController < Api::V1::ApiController
   def index
-    # response.set_header("Content-Type", "application/vnd.api+json")
-
     @tasks = Task.all
   end
 
   def show
-    # response.set_header("Content-Type", "application/vnd.api+json")
-
     @task = Task.find(params[:id])
   end
 
   def create
-    # puts "----"
-    # pp task_params
-    # puts "----"
-
     @task = Task.new(task_params)
-    if @task.save
-      render json: { result: :success }, status: :created
-    else
-      render json: { result: :fail, errors: @task.errors.full_messages }, status: :unprocessable_entity
-    end
+    @task.save!
 
-    # debugger
+    render json: { result: :success }, status: :created
+  rescue ActiveRecord::RecordInvalid
+    render json: { result: :fail, errors: @task.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+
+    render json: { result: :success }, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { result: :fail }, status: :not_found
+  rescue ActiveRecord::RecordInvalid
+    render json: { result: :fail, errors: @task.errors.full_messages }, status: :unprocessable_entity
   end
 
   def destroy
