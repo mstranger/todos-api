72e470a0ce41050eab6a6f48e7a582f80c47b48c
mstranger <mr.mstranger@gmail.com>
2023-04-15 17:27:24 +0300

    User model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,7 +12,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(email: "foo@bar.com", password: "password")
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "invalid without email" do
+    @user.email = ""
+    refute @user.valid?
+    refute_nil @user.errors.messages.fetch(:email, nil)
+  end
+
+  test "invalid without password" do
+    @user.password = nil
+    refute @user.valid?
+    refute_nil @user.errors.messages.fetch(:password, nil)
+  end
+
+  test "invalid with existing email" do
+    @user.email = "jdoe@mail.com"
+    refute @user.valid?
+  end
 end
