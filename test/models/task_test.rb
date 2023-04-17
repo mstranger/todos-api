a09d872edd017110d62ec3858956f0e33fe0b1a7
mstranger <mr.mstranger@gmail.com>
2023-04-17 22:55:36 +0300

    Task model tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -13,7 +13,30 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = Task.new(title: "example")
+  end
+
+  test "valid task" do
+    assert @task.valid?
+  end
+
+  test "invalid without title" do
+    @task.title = ""
+    refute @task.valid?
+    refute_nil @task.errors.messages.fetch(:title, nil)
+  end
+
+  test "invalid with length less then 3" do
+    @task.title = "ab"
+    refute @task.valid?, "too short"
+
+    @task.title = "abc"
+    assert @task.valid?
+  end
+
+  test "invalid with existing title" do
+    @task.title = tasks(:one).title
+    refute @task.valid?
+  end
 end
