9a735dbadba181309bf65ae5817d6c0fb2d7d109
mstranger <mr.mstranger@gmail.com>
2023-04-19 15:59:49 +0300

    Clean integration tests folder

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class AuthenticationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup { @user = users(:john) }
+
+  test "login success" do
+    token = authenticate! @user
+    data = decode(token)
+
+    assert_equal 200, response.status
+    assert_equal @user.id, data["user_id"]
+  end
+
+  test "login fail" do
+    @user.email = "other@mail.com"
+    authenticate! @user
+
+    assert_equal 401, response.status
+    assert_equal t("user.errors.auth_fail"), parse_resp["error"]
+  end
 end
