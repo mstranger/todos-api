ee8bcc4ac8e1a5b7abb414edbf1e0da35a7233fb
mstranger <mr.mstranger@gmail.com>
2023-04-26 16:39:18 +0300

    Routes file edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   apipie
 
-  post "/users", to: "users#create"
-  match "/users", to: "users#update", via: [:put, :patch]
   get "/me", to: "users#me"
+  post "/users", to: "users#create"
+  match "/users", to: "users#update", via: %i[put patch]
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
       # TODO: shallow ?
-
       resources :projects do
         resources :tasks do
-          resources :comments, only: [:index, :create, :destroy]
+          resources :comments, only: %i[index create destroy]
 
           post "toggle", on: :member
         end
