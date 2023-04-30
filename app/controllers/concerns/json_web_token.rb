0b0f54a9a1750b9b3c83cfb748e6b21fc9edf359
mstranger <mr.mstranger@gmail.com>
2023-04-30 13:19:51 +0300

    Fix brakeman warn

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -3,7 +3,7 @@ module JsonWebToken
 
   SECRET_KEY = Rails.application.secret_key_base
 
-  def jwt_encode(payload, exp = 7.days.from_now)
+  def jwt_encode(payload, exp = 30.days.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
