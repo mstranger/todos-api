e20a90e6d232b62bae47b2bf414caedd4800648f
mstranger <mr.mstranger@gmail.com>
2023-05-19 17:49:35 +0300

    Add user email to jwt payload

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -17,7 +17,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by(email: params[:email])
 
     if @user&.authenticate(params[:password])
-      token = jwt_encode(user_id: @user.id)
+      token = jwt_encode(user_id: @user.id, email: @user.email)
       render json: {token: token}, status: :ok
     else
       render json: {error: t("user.errors.auth_fail")}, status: :unauthorized
